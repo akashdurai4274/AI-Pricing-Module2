6eccc62340f2d0846f9986b0d66d44bcaa9f7faf
AkashDurai <93862553+akashdurai4274@users.noreply.github.com>
2025-03-19 17:54:22 +0530

    Update hero-section.tsx

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -5,8 +5,8 @@ import Star from "./Star";
 export default function HeroSection() {
   return (
     <section className="py-16 text-center relative">
-      <div className="absolute top-0 left-0 w-full h-full">
-        <div className="absolute top-10 left-20"></div>
+      <div className="absolute top-[-110px] left-[0px] right-0 flex justify-center items-center  ">
+        <img src="/border.png" alt="border" className="bw-[38%] h-full " />
       </div>
 
       <div className="max-w-4xl mx-auto ">
@@ -19,7 +19,6 @@ export default function HeroSection() {
           <span className="bg-blue-100 bg-opacity-60  text-blue-700 px-2 py-1  rounded">
             Priced to fit your budget
           </span>
-          {/* className="bg-blue-100 text-blue-600 px-2 py-1 rounded" */}
         </h1>
         <p className="text-lg text-gray-600">
           Choose from affordable pricing plans tailored to suit your needs
